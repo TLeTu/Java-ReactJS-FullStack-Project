4ccc65d5cf94adcaabe7ea928a051b7f910484d6
tranletu2003 <tranletu2003@gmail.com>
2025-04-22 16:43:10 +0700

    add mongodb dependencies

--- backend/src/main/java/dev/_6/JavaProject/JavaProjectApplication.java
+++ backend/src/main/java/dev/_6/JavaProject/JavaProjectApplication.java
@@ -2,6 +2,8 @@ package dev._6.JavaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JavaProjectApplication {
