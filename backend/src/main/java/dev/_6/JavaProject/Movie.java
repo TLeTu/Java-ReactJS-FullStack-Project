f1a1f020fea0eca015b4e5e3abfca6dccf39fba1
tranletu2003 <tranletu2003@gmail.com>
2025-04-22 21:51:19 +0700

    All working

--- backend/src/main/java/dev/_6/JavaProject/Movie.java
+++ backend/src/main/java/dev/_6/JavaProject/Movie.java
@@ -25,5 +25,5 @@ public class Movie {
     private List<String> genres;
     private List<String> backdrops;
     @DocumentReference
-    private List<String> reviewIds;
+    private List<Review> reviewIds;
 }
