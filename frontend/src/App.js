7a29f025e4f5c9f5efd38608f761bde4cd466c54
tranletu2003 <tranletu2003@gmail.com>
2025-04-23 14:48:04 +0700

    Routing frontend
    Setup layout and routing logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './api/AxiosConfig';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Layout from './components/layout';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  }
+  useEffect(() => {
+    getMovies();
+  }
+    , []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+           <Route path="/" element={<Home/>}>
+           </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container mt-5">
+        <h1>Welcome to the Movie App</h1>
+        <p>Discover your favorite movies and TV shows.</p>
+        </div>
+    );
+    }
+export default Home;
